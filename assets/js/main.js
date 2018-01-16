3cbd9eab41b54d98158174c6ab5fcd9318210f49
Yerbol Syzdyk <erbcreedok@gmail.com>
2018-01-16 10:02:17 +0300

    news block animation added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+/* Gallery slider animation */
 jQuery(document).ready(function($) {
 
     var galleryCarousel = $('#galleryCarousel');
@@ -32,4 +33,64 @@ jQuery(document).ready(function($) {
     });
 
     updateActiveThumb();
+});
+
+/* News block animation*/
+jQuery(document).ready(function($) {
+
+    var time = 10000;
+    var newsMain = $("#news-main");
+    var newsList = $("#news-list .news-media");
+    var active = 0;
+    var timeout;
+
+    var style=document.createElement('style');
+    var cssStyle = ' .news-block .news-sidebar .media.active:before { -webkit-animation-duration: ' + time + 'ms; -moz-animation-duration: ' + time + 'ms; -o-animation-duration: ' + time + 'ms; animation-duration: ' + time + 'ms;} ';
+    style.type='text/css';
+    if(style.styleSheet){
+        style.styleSheet.cssText=cssStyle;
+    }else{
+        style.appendChild(document.createTextNode(cssStyle));
+    }
+    document.getElementsByTagName('head')[0].appendChild(style);
+
+
+    function getNews(elem) {
+        return {
+            image: elem.find('img').attr('src'),
+            heading: elem.find('.media-heading').html(),
+            paragraph: elem.find('p').html(),
+            elem: elem
+        }
+    }
+    function setActiveNews(elem) {
+        newsList.removeClass('active');
+        elem.addClass('active');
+    }
+    function renderMainNews(data) {
+        newsMain.find('img').attr('src',data.image);
+        newsMain.find('.heading').html(data.heading);
+        newsMain.find('.paragraph').html(data.paragraph);
+        newsMain.trigger('news-changed');
+    }
+    function onSwitchNews(number) {
+        var news = newsList.eq(number);
+        renderMainNews(getNews(news));
+        setActiveNews(news);
+    }
+
+    function startTimeout() {
+        timeout = setTimeout(function(){
+            active += 1 ;
+            active %= newsList.length;
+            onSwitchNews(active);
+        }, time);
+    }
+
+    $(newsMain).on('news-changed', function(){
+        clearTimeout(timeout);
+        startTimeout();
+    });
+
+    startTimeout();
 });
